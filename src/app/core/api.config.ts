8e31a7e363ce935a5f020b82e3c8c40647e26705
LluisArjuna <lluisarjunav@gmail.com>
2026-04-13 10:51:49 +0200

    refactor(api-config): we delete the api key interfactor because it was causing problems

--- src/app/core/api.config.ts
+++ src/app/core/api.config.ts
@@ -1,16 +1,5 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { apiKeyInterceptor } from './api-key.interceptor';
-
-export const appConfig = {
-  providers: [
-    provideHttpClient(
-      withInterceptors([apiKeyInterceptor])
-    )
-  ]
-};
-
 export const API_CONFIG = {
   baseUrl: 'https://api.themoviedb.org/3',
   tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/',
-  apiKey: 'YOUR_API_KEY'
+  apiKey: 'af4f610983ac7089ae020e8596178b0d'
 };
