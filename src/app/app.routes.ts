4bed701abbcef8f1408eb9abf2319b4f36743257
LluisArjuna <lluisarjunav@gmail.com>
2026-04-13 10:55:13 +0200

    feat(app-routes): add the routes we will use in the app add html structure to app.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MovieListComponent } from './features/movies/pages/movie-list-component/movie-list-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MovieListComponent},
+    {path: 'movies', component: MovieListComponent},
+    /*
+    {path: 'movies/:movieId', component: MovieDetails},
+    {path: 'favorites', component: FavoriteMoviesComponent},
+*/
+    {path: '**', redirectTo: ''}
+];
