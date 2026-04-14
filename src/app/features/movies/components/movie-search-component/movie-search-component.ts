a13f4cff061cbcd3c021cd1fcd67f08b10b625b8
LluisArjuna <lluisarjunav@gmail.com>
2026-04-14 10:25:27 +0200

    refactor(movie-serch): fix the movie-search component

--- src/app/features/movies/components/movie-search-component/movie-search-component.ts
+++ src/app/features/movies/components/movie-search-component/movie-search-component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, output, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
 
@@ -14,18 +14,23 @@ export class MovieSearchComponent {
   search = output<string>();
   query = signal('');
 
-  private searchSubject = new Subject<string>();
+  private debounceTimeout: any;
 
-  // input handler
+  private searchEffect = effect(() => {
+    const value = this.query();
+
+    clearTimeout(this.debounceTimeout);
+
+    this.debounceTimeout = setTimeout(() => {
+      this.search.emit(value);
+    }, 400);
+  });
+
   onInput(value: string) {
     this.query.set(value);
-    this.searchSubject
-    .pipe(debounceTime(300), distinctUntilChanged())
-    .subscribe(value => this.search.emit(value));
   }
 
   clear() {
     this.query.set('');
-    this.searchSubject.next('');
   }
 }
