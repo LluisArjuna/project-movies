d44e7bdc5f6fe251bcf5819abad8a4a64f3782a0
LluisArjuna <lluisarjunav@gmail.com>
2026-04-20 13:08:24 +0200

    core(env): add api and database information in env file
    Declare this information in api.config and app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,3 +11,13 @@ import { Header } from './core/layout/header/header';
 export class App {
   protected readonly title = signal('project-movies');
 }
+
+const firebaseConfig = {
+  apiKey: '__FIREBASE_API_KEY__',
+  authDomain: '__FIREBASE_AUTH_DOMAIN__',
+  projectId: '__FIREBASE_PROJECT_ID__',
+  storageBucket: '__FIREBASE_STORAGE_BUCKET__',
+  messagingSenderId: '__FIREBASE_MESSAGING_SENDER_ID__',
+  appId: '__FIREBASE_APP_ID__',
+  measurementId: '__FIREBASE_MEASUREMENT_ID__'
+};
--- src/app/core/api.config.ts
+++ src/app/core/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
   baseUrl: 'https://api.themoviedb.org/3',
   tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/',
-  apiKey: 'af4f610983ac7089ae020e8596178b0d'
+  apiKey: '__TMDB_API_KEY__'
 };
