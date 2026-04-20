cf8fbc9c66f120ac73d634bccdeb9746643cfcbd
LluisArjuna <lluisarjunav@gmail.com>
2026-04-20 13:10:30 +0200

    refator(environment): move main to environment folder

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,24 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
+import { environment } from '@environments/environment';  
+
+import { initializeApp } from "firebase/app"; 
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth } from "@angular/fire/auth";
+import { provideFirestore } from '@angular/fire/firestore';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(()=>getFirestore()),
   ]
-};
+};  
+ 
