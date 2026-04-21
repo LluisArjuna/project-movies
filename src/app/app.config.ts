893d19a75c63e274c6df0f81f30acf20c02679b7
LluisArjuna <lluisarjunav@gmail.com>
2026-04-21 13:38:23 +0200

    feat(env): add keys hardcoded in api and app config because .env and environment is not working at the moment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,14 +4,25 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
-import { environment } from '@environments/environment';  
+import { environment } from '../environments/environment';
 
 import { initializeApp } from "firebase/app"; 
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
+
 import { provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth } from "@angular/fire/auth";
 import { provideFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBUn76r7Y2Dx3jFgum0IVEJtKWyzqWvHG4",
+  authDomain: "movies-112233.firebaseapp.com",
+  projectId: "movies-112233",
+  storageBucket: "movies-112233.firebasestorage.app",
+  messagingSenderId: "347288037329",
+  appId: "1:347288037329:web:6e5daec56a4e31a3ef22f0",
+  measurementId: "G-FJGT03HB7Y"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,7 +30,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(()=>getFirestore()),
   ]
