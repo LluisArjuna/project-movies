91cf27a44b59e41fa27d98bc106341693c6c919f
LluisArjuna <lluisarjunav@gmail.com>
2026-04-22 10:52:13 +0200

    feat(auth): implement registration page
    - add reactive form with name, email and password validation
    - integrate AuthService register method
    - handle navigation to login after successful registration
    - log registration errors

---
+++ src/app/features/firestore/components/register/register.ts
@@ -0,0 +1,37 @@
+import { Component, inject, ChangeDetectionStrategy} from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/firestore';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink],
+  templateUrl: './register.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegisterComponent {
+
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  form = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  async register() {
+    if (this.form.invalid) return;
+
+    const { name, email, password } = this.form.value;
+
+    try {
+      await this.authService.register(email!, password!, name!);
+      await this.router.navigate(['/login']);
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  }
+}
