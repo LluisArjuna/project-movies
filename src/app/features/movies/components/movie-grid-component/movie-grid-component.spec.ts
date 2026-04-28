024b522f166eb1f01a1e7a7ef71b80875b89b24f
LluisArjuna <lluisarjunav@gmail.com>
2026-04-28 11:18:16 +0200

    test(movie-grid): add tests for movie-grid-component

--- src/app/features/movies/components/movie-grid-component/movie-grid-component.spec.ts
+++ src/app/features/movies/components/movie-grid-component/movie-grid-component.spec.ts
@@ -1,22 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MovieGridComponent } from './movie-grid-component';
 
 describe('MovieGridComponent', () => {
   let component: MovieGridComponent;
   let fixture: ComponentFixture<MovieGridComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [MovieGridComponent],
-    }).compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [MovieGridComponent]
+    });
 
     fixture = TestBed.createComponent(MovieGridComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render movie cards', () => {
+    fixture.componentRef.setInput('movie', {
+      id: 1,
+      title: 'Test',
+      posterUrl: '',
+      backdropUrl: null,
+      rating: 0,
+      releaseDate: '',
+      overview: ''
+    });
+
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('app-movie-card')).toBeTruthy();
   });
 });
