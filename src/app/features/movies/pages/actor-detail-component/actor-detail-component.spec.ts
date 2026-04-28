5336fe340a9cd9445cbe87d9fcc9b69124661e9e
LluisArjuna <lluisarjunav@gmail.com>
2026-04-28 11:18:55 +0200

    test(actor-detail): add tests for actor-detail-component

--- src/app/features/movies/pages/actor-detail-component/actor-detail-component.spec.ts
+++ src/app/features/movies/pages/actor-detail-component/actor-detail-component.spec.ts
@@ -1,22 +1,57 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 import { ActorDetailComponent } from './actor-detail-component';
+import { MovieService } from '../../services/movie-service';
 
 describe('ActorDetailComponent', () => {
   let component: ActorDetailComponent;
   let fixture: ComponentFixture<ActorDetailComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       imports: [ActorDetailComponent],
-    }).compileComponents();
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => '1'
+              }
+            }
+          }
+        },
+        {
+          provide: MovieService,
+          useValue: new MovieService()
+        }
+      ]
+    });
 
     fixture = TestBed.createComponent(ActorDetailComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load actor data on init', () => {
+    const actor = component.actor();
+
+    expect(actor).not.toBeNull();
+    expect(actor?.name).toBe('Actor Name');
+  });
+
+  it('should load actor movies', () => {
+    expect(component.movies().length).toBe(1);
+    expect(component.movies()[0].title).toBe('Movie');
+  });
+
+  it('should set loading to false after fetch', () => {
+    expect(component.loading()).toBe(false);
   });
 });
